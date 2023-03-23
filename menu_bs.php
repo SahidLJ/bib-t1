b50f34a4c2972f5e42dc3c297e3904987b889de0
SahidLJ <128098078+SahidLJ@users.noreply.github.com>
2023-03-22 21:29:15 -0300

    Update menu_bs.php

--- menu_bs.php
+++ menu_bs.php
@@ -7,14 +7,14 @@ session_start();
    <title>BASES PWD</title>
    <meta charset="utf-8">
    <meta name="viewport" content="width=device-width, initial-scale=1">
-   <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" >
-   <script src="bootstrap/js/jquery-3.1.0.min.js"></script>
-   <script src="bootstrap/js/bootstrap.min.js"></script>
+   <!--<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" > -->
+   <!--<script src="bootstrap/js/jquery-3.1.0.min.js"></script> -->
+   <!--<script src="bootstrap/js/bootstrap.min.js"></script> -->
    <script src="bootstrap/js/funciones_gral.js"></script>
    <link rel="stylesheet" href="bootstrap/css/style_chat.css" media="all"/>	
-   <link rel="stylesheet" href="bootstrap/ui/jquery-ui.css">
+   <!--<link rel="stylesheet" href="bootstrap/ui/jquery-ui.css"> -->
    <link rel="stylesheet" href="bootstrap/cust.css">
-   <script src="bootstrap/ui/jquery-ui.js"></script>
+   <!--<script src="bootstrap/ui/jquery-ui.js"></script> -->
      
    <!-----https://sourcecodesite.com/how-to-create-chat-system-in-php-using-ajax-2.html--->
    <!--Include Custom CSS-->
